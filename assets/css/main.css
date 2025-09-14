7bc6c2c0e7eead95eb11eda5a6944e7f5773adf5
chaar-code <alshaar211@gmail.com>
2025-09-14 15:05:09 +0300

    Replace NiceShop with perfumer across all pages and files

--- assets/css/main.css
+++ assets/css/main.css
@@ -1,6 +1,6 @@
 /**
-* Template Name: NiceShop
-* Template URL: https://bootstrapmade.com/niceshop-bootstrap-ecommerce-template/
+* Template Name: perfumer
+* Template URL: https://bootstrapmade.com/perfumer-bootstrap-ecommerce-template/
 * Updated: Aug 26 2025 with Bootstrap v5.3.7
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
